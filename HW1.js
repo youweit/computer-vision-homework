a40bc371315474d55052b4bd7d3b7eca6f29ede5
Youwei Teng <ranyouwei3@gmail.com>
2015-09-27 22:01:07 +0800

    update tab indent.

--- HW1.js
+++ HW1.js
@@ -1,37 +1,37 @@
 var cv = require("opencv"),
-		utils = require("./utils")
+    utils = require("./utils")
 
 function main () {
 
-	//read lena image from the file system using opencv
-	cv.readImage('image/lena.bmp', function(err, inputMat){
+  //read lena image from the file system using opencv
+  cv.readImage('image/lena.bmp', function(err, inputMat){
 
-		var upsideDown = inputMat.copy(),
-				rightLeft = inputMat.copy(),
-				diagonal = inputMat.copy(),
-				height = inputMat.height(),
-				width = inputMat.width()
+    var upsideDown = inputMat.copy(),
+        rightLeft = inputMat.copy(),
+        diagonal = inputMat.copy(),
+        height = inputMat.height(),
+        width = inputMat.width()
 
-		console.log("input image =", width, " x ",height)
+    console.log("input image =", width, " x ",height)
 
-		/**
-		 * Use matrix.pixel(i, j) to get the pixel
-		 *     matrix.pixel(i, j, [r,g,b]) to set the pixel
-		 **/
-		for (var i = 0; i < height; i++) {
-			for (var j = 0; j < width; j++) {
-				upsideDown.pixel(i, j, inputMat.pixel(height-i-1, j))
-				rightLeft.pixel(i, j, inputMat.pixel(i, height-j-1))
-				diagonal.pixel(i, j, inputMat.pixel(j, i)) //invert x and y
-			}
-		}
+    /**
+     * Use matrix.pixel(i, j) to get the pixel
+     *     matrix.pixel(i, j, [r,g,b]) to set the pixel
+     **/
+    for (var i = 0; i < height; i++) {
+      for (var j = 0; j < width; j++) {
+        upsideDown.pixel(i, j, inputMat.pixel(height-i-1, j))
+        rightLeft.pixel(i, j, inputMat.pixel(i, height-j-1))
+        diagonal.pixel(i, j, inputMat.pixel(j, i)) //invert x and y
+      }
+    }
 
-		upsideDown.save('./output/HW1/HW1_upside_down.bmp')
-		rightLeft.save('./output/HW1/HW1_right_left.bmp')
-		diagonal.save('./output/HW1/HW1_diagonal.bmp')
-		console.log('finished!')
-		// utils.showMatrixOnWindow(diagonal)
-	})
+    upsideDown.save('./output/HW1/HW1_upside_down.bmp')
+    rightLeft.save('./output/HW1/HW1_right_left.bmp')
+    diagonal.save('./output/HW1/HW1_diagonal.bmp')
+    console.log('finished!')
+    // utils.showMatrixOnWindow(diagonal)
+  })
 }
 
 main()
