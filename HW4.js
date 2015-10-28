f832b41f4de0daa36fc0812711d846a5bf8a4f38
youweit <teng@dcard.cc>
2015-10-28 17:28:05 +0800

    add erosion, opening, closing.

--- HW4.js
+++ HW4.js
@@ -7,6 +7,7 @@ function Kernel(kernel, origin) {
   this.kernel = kernel
   this.origin = origin
 
+  //this calcutate the relative postition for the kernel. only present the 1 one.
   this.points = []
   for (let x = 0; x < kernel.length; x++) {
     for (let y = 0; y < kernel[x].length; y++) {
@@ -28,15 +29,16 @@ function applyDilation(inputMat, kernel) {
     for (let j = 0; j < inputMat.width(); j++) { //this is image width
       if (inputMat.pixelValueAt(i,j) == whitePixel[0]) {
         for (let kernelIndex = 0; kernelIndex < kernel.points.length; kernelIndex++) {
-          if (j + kernel.points[kernelIndex].x >= 0 && j + kernel.points[kernelIndex].x <= inputMat.width() 
-            && i + kernel.points[kernelIndex].y >= 0 && i + kernel.points[kernelIndex].y <= inputMat.height()) {
-            pixelsToModify.push({row: i + kernel.points[kernelIndex].y, col: j + kernel.points[kernelIndex].x})
+          let x = j + kernel.points[kernelIndex].x
+          let y = i + kernel.points[kernelIndex].y
+          if (x >= 0 && x <= inputMat.width() && y >= 0 && y <= inputMat.height()) {
+            pixelsToModify.push({row: y, col: x})
           }
         }
       }
     }
   }
-
+  
   //apply the effect
   for (let pixel of pixelsToModify) {
     result.pixel(pixel.row, pixel.col, whitePixel)
@@ -44,6 +46,53 @@ function applyDilation(inputMat, kernel) {
 
   return result
 }
+
+function applyErosion(inputMat, kernel) {
+
+  let result = inputMat.copy()
+  let whitePixel = [255, 255, 255]
+  let blackPixel = [0, 0, 0]
+
+  for (let i = 0; i < inputMat.height(); i++) { //this is image height
+    for (let j = 0; j < inputMat.width(); j++) { //this is image width
+      if (inputMat.pixelValueAt(i,j) === whitePixel[0]) {
+        let erosion = true
+        for (let kernelIndex = 0; kernelIndex < kernel.points.length; kernelIndex++) {
+          let x = j + kernel.points[kernelIndex].x
+          let y = i + kernel.points[kernelIndex].y
+
+          if (x >= 0 && x <= inputMat.width() && y >= 0 && y <= inputMat.height()) {
+            //pixelValueAt(col, row)
+            if (inputMat.pixelValueAt(y,x) !== whitePixel[0]) {
+              erosion = false
+              break;
+            }
+          } else {
+            erosion = false
+          }
+        }
+        if (erosion) {
+          result.pixel(i, j, whitePixel)
+        } else {
+          result.pixel(i, j, blackPixel)
+        }
+      }
+    }
+  }
+
+
+  return result
+}
+
+
+function applyOpening(inputMat, kernel) {
+  return applyDilation(applyErosion(inputMat, kernel), kernel)
+}
+
+function applyClosing(inputMat, kernel) {
+  return applyErosion(applyDilation(inputMat, kernel), kernel)
+}
+
 
 function main() {
   //read lena image from the file system using opencv
@@ -55,27 +104,37 @@ function main() {
     }
 
     var dilationMat = inputMat.copy(), 
-        erosion = inputMat.copy(),
-        opening = inputMat.copy(),
-        closing = inputMat.copy(),
+        erosionMat = inputMat.copy(),
+        openingMat = inputMat.copy(),
+        closingMat = inputMat.copy(),
         height = inputMat.height(),
         width = inputMat.width(),
         totalPixels = height * width
     console.log('input image = ', width, ' x ',height)
 
     let octogonalKenel = [
-      [0,1,1,1,0],
-      [1,1,1,1,1],
-      [1,1,1,1,1],
-      [1,1,1,1,1],
-      [0,1,1,1,0]
-    ] //center is 2,2,
+      [0, 1, 1, 1, 0],
+      [1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1],
+      [0, 1, 1, 1, 0]
+    ] //origin is 2,2,
 
     //convert the input image to binary.
     inputMat = utils.binarized(inputMat)
 
     dilationMat = applyDilation(inputMat, new Kernel(octogonalKenel, {x: 2, y: 2}))
     dilationMat.save('./output/HW4/HW4_dilation.bmp')
+
+    // erosionMat = applyErosion(inputMat, new Kernel(octogonalKenel, {x: 2, y: 2}))
+    // erosionMat.save('./output/HW4/HW4_erosion.bmp')
+
+    // openingMat = applyOpening(inputMat, new Kernel(octogonalKenel, {x: 2, y: 2}))
+    // openingMat.save('./output/HW4/HW4_opening.bmp')
+
+    // closingMat = applyClosing(inputMat, new Kernel(octogonalKenel, {x: 2, y: 2}))
+    // closingMat.save('./output/HW4/HW4_closing.bmp')
+
     utils.showMatrixOnWindow(dilationMat)
   })
 }
